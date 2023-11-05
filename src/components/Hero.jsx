3b9c517f2c843bd47f07430d10acd58064ac0f34
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2023-11-05 18:44:02 +0000

    Styling changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
       {/* scroll GIF  */}
       <div className="absolute flex items-center justify-center w-full xs:bottom-10 bottom-32">
         <a href="#about">
-          <div className="w-[28px] h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[28px] h-[48px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
               animate={{ y: [0, 24, 0] }}
               transition={{
