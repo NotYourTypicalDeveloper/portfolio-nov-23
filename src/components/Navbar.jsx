9a0b2a7df6f61bbc1939a22dc6c973e7661a757c
NotYourTypicalDeveloper <carolineho1983@gmail.com>
2023-11-05 16:06:03 +0000

    Finished navbar desktop & mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
-        {/* Logo__ */}
+        {/* My logo__ */}
         <Link
           to="/"
           className="flex items-center gap-2"
@@ -24,10 +24,10 @@ const Navbar = () => {
         >
           <img src={logo2} alt="logo" className="object-contain w-10 h-10" />
 
-          <p className="text-white">Caroline Ho</p>
+          <p className="font-bold text-white">Caroline Ho</p>
         </Link>
 
-        {/* navbar links__ */}
+        {/* Navbar links__ */}
         <ul className="flex-row hidden gap-10 list-none sm:flex">
           {navLinks.map((link) => (
             <li
@@ -43,7 +43,9 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        {/* Burger menu mobile view 🍔 */}
+
+        {/* MOBILE VIEW 📱 ===========  */}
+        {/* Burger menu icon ___ */}
         <div className="flex items-center justify-end flex-1 sm:hidden">
           <img
             src={toggle ? close : menu}
@@ -51,6 +53,32 @@ const Navbar = () => {
             className="w-[28] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
+        </div>
+
+        {/* Menu itself___  */}
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w [140px] z-10 rounded-xl`}
+        >
+          <ul className="flex flex-col items-start justify-end gap-4 list-none ">
+            {navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } font-poppins text-[16px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(link.title);
+                }}
+              >
+                <a href={`#${link.id}`} className="ref">
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
